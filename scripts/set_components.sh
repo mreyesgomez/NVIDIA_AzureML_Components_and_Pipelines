30d170e66ffee753bb855c9687aa96dbe7016b17
mreyesgomez <mreyesgomez@nvidia.com>
2023-02-21 12:10:01 -0800

    Default Triton Components need to be created first not to break the creation of PipelineComponents

--- scripts/set_components.sh
+++ scripts/set_components.sh
@@ -2,6 +2,16 @@
 
 source scripts/config_files/config.sh
 
+
+##Create Triton Components by Default
+export COMPONENTS_DIR=components/triton/
+
+for component_file in $(find $COMPONENTS_DIR -name '*.yml');
+do
+    echo "az ml component create --file ${component_file}"
+    az ml component create --file ${component_file};
+done;
+
 export COMPONENTS_DIR=components/${nvidia_product}/${container}
 
 for component_file in $(find $COMPONENTS_DIR -name '*.yml');
@@ -10,10 +20,3 @@ do
     az ml component create --file ${component_file};
 done;
 
-export COMPONENTS_DIR=components/triton/
-
-for component_file in $(find $COMPONENTS_DIR -name '*.yml');
-do
-    echo "az ml component create --file ${component_file}"
-    az ml component create --file ${component_file};
-done;
