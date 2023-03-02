05f8ea8de8df10b57f8a08a5a8932473fe31b168
mreyesgomez <mreyesgomez@nvidia.com>
2023-03-01 17:45:33 -0800

    adding logic to create pipeline components after all command components have been created

--- scripts/set_components.sh
+++ scripts/set_components.sh
@@ -14,9 +14,21 @@ done;
 
 export COMPONENTS_DIR=components/${nvidia_product}/${container}
 
+SUB='pipeline'
+###pipeline components should be created after all regular command components have been created
 for component_file in $(find $COMPONENTS_DIR -name '*.yml');
 do
-    echo "az ml component create --file ${component_file}"
-    az ml component create --file ${component_file};
+    if [[ "$component_file" != *"$SUB"* ]]; then
+        echo "az ml component create --file ${component_file}"
+        az ml component create --file ${component_file};
+    fi
+done;
+
+for component_file in $(find $COMPONENTS_DIR -name '*.yml');
+do
+    if [[ "$component_file" == *"$SUB"* ]]; then
+        echo "az ml component create --file ${component_file}"
+        az ml component create --file ${component_file};
+    fi
 done;
 
